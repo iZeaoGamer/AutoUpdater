712e764a912305bf3a11559bb5ee501fa6c66eea
iZeaoGamer <jameshayward14@icloud.com>
2018-03-12 17:49:48 +0000

    Update Commands.php

--- src/AutoUpdater/Commands/Commands.php
+++ src/AutoUpdater/Commands/Commands.php
@@ -27,7 +27,7 @@ class Commands extends PluginBase implements CommandExecutor{
         $this->plugin = $plugin;
     }
     
-    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : void {
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
     	$fcmd = strtolower($cmd->getName());
     	switch($fcmd){
     		case "autoupdater":
